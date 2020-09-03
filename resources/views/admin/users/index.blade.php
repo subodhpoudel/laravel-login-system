0b4a6b46c5eb83eb0ae0b28102fff4fa8082c12d
subodhpoudel <subodhcj@gmail.com>
2020-09-03 06:39:30 +0545

    table added in index view and links for edit and delete

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,9 +8,31 @@
                 <div class="card-header">{{ __('Users') }}</div>
 
                 <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                     @foreach($users as $user)
-                    {{ $user->name }} - {{ $user->email }}
+                        <tr>
+                        <th scope="row">{{  $user->id }}</th>
+                        <td>{{  $user->name }}</td>
+                        <td>{{  $user->email }}</td>
+                        <td>
+                        <a href ="{{ route('admin.users.edit', $user->id) }}"> <button type ="button" class="btn btn-primary"> Edit </button></a>
+                        <a href ="{{ route('admin.users.destroy', $user->id) }}"> <button type ="button" class="btn btn-warning"> Delete </button></a>
+                        </td>
+                        
+                        </tr>
                     @endforeach
+                    </tbody>
+                </table>
                 </div>
             </div>
         </div>
