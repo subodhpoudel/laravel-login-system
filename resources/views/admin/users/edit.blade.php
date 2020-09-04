e233a8ae58c39fd8f0427e1ef1dc1d2fe35eb430
subodhpoudel <subodhcj@gmail.com>
2020-09-04 17:16:35 +0545

    checked existin roles in view

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -13,7 +13,8 @@
                     {{ method_field('PUT') }}
                     @foreach($roles as $role)
                         <div class="form-check">
-                            <input type = "checkbox" name ="roles[]" value = " {{ $role->id }}">
+                            <input type = "checkbox" name ="roles[]" value = " {{ $role->id }}"
+                            @if($user->roles->pluck('id')->contains($role->id)) checked @endif >
                             <lablel> {{ $role->name }} </label>
                         </div>
                     @endforeach
