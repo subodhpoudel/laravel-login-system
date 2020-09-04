f57b3ae940401f28246040b41538f6b34fef0177
subodhpoudel <subodhcj@gmail.com>
2020-09-04 20:34:03 +0545

    redirection with roles

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -36,5 +36,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectTo() {
+        if(Auth::user()->hasRole('admin')) {
+            $this->redirectTo = route('admin.users.index');
+            return $this->redirectTo;
+        }
+        return $this->redirectTo =route('home'); 
     }
 }
